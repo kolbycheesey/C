7cd7fd02de1a62438e378a936c67c16c7fcdf024
kolbycheesey <m.kolb.doc@gmail.com>
2024-04-08 18:08:44 -0400

    udpate

--- C#/appTest/obj/Debug/net8.0/appTest.AssemblyInfo.cs
+++ C#/appTest/obj/Debug/net8.0/appTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+937c36696597750599f710a4dacc7e64f7a8868c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df00118f08e23b119baebf13dbc56b6fce55c6a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("appTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- C#/obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ C#/obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+937c36696597750599f710a4dacc7e64f7a8868c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df00118f08e23b119baebf13dbc56b6fce55c6a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
